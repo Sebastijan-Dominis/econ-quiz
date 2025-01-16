e203cdbbd665cf8bfdf96fd43e0542a384b83070
Sebastijan Dominis <sebastijan.dominis99@gmail.com>
2025-01-16 23:01:31 +0100

    Configured tailwind a bit.

--- econ_quiz/src/App.vue
+++ econ_quiz/src/App.vue
@@ -2,36 +2,15 @@
 </script>
 
 <template>
-  <div class="background-container">
-    <img src="../src/assets/images/background.jpg" class="background-image" />
-    <div class="cover"></div>
+  <div class="relative min-h-screen p-5">
+    <!-- Background Image -->
+    <img src="../src/assets/images/background.jpg" class="fixed top-0 left-0 w-full h-full object-cover z-[-2]" />
+    <!-- Semi-Transparent Overlay -->
+    <div class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 z-[-1]"></div>
   </div>
 </template>
 
+
 <style scoped>
-.background-container {
-  position: relative;
-  min-height: 100vh; 
-  padding: 20px; 
-}
 
-.background-image {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  z-index: -2; 
-}
-
-.cover {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.4); 
-  z-index: -1; 
-}
 </style>
--- econ_quiz/tailwind.config.js
+++ econ_quiz/tailwind.config.js
@@ -3,11 +3,45 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // brand
+        'brand': '#ffa550',
+        'shade': {
+          '100': '#e69500',
+          '200': '#cc8400',
+          '300': '#b37300',
+          '400': '#996300',
+          '500': '#805300',
+          '600': '#664200',
+          '700': '#4d3100',
+          '800': '#332100',
+          '900': '#191000',
+        },
+        'tint': {
+          '100': '#ffae1a',
+          '200': '#ffb733',
+          '300': '#ffc04d',
+          '400': '#ffc966',
+          '500': '#ffd280',
+          '600': '#ffdb99',
+          '700': '#ffe4b3',
+          '800': '#ffedcc',
+          '900': '#fff6e6',
+        },
+
+        // grays
+        'gray': '#808080',
+        'wg': '#e6e6e6',
+        'bg': '#1a1a1a',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'Arial', 'sans-serif']
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
-
