b449829379d3633f08b7a8a2a68c18221fb014d6
Sebastijan Dominis <sebastijan.dominis99@gmail.com>
2025-01-16 21:26:22 +0100

    Added some images, and created the background.

--- econ_quiz/src/App.vue
+++ econ_quiz/src/App.vue
@@ -1,11 +1,37 @@
 <script>
-
 </script>
 
 <template>
-  
+  <div class="background-container">
+    <img src="../src/assets/images/background.jpg" class="background-image" />
+    <div class="cover"></div>
+  </div>
 </template>
 
-<style>
+<style scoped>
+.background-container {
+  position: relative;
+  min-height: 100vh; 
+  padding: 20px; 
+}
 
+.background-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -2; 
+}
+
+.cover {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4); 
+  z-index: -1; 
+}
 </style>
