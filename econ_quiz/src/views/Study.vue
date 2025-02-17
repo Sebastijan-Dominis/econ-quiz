f5767cde55c357a05085740a31af7fda7034bd8b
Sebastijan Dominis <sebastijan.dominis99@gmail.com>
2025-02-17 23:07:06 +0100

    the tailwind for the study view has been changed, so as to make it look better

--- econ_quiz/src/views/Study.vue
+++ econ_quiz/src/views/Study.vue
@@ -21,9 +21,9 @@ watchEffect(() => {
     <div v-if="storeStudy.loading">Loading...</div>
     <div v-if="storeStudy.error">{{ storeStudy.error }}</div>
     <div v-if="!storeStudy.loading && !storeStudy.error">
-      <h2 class="flex justify-center text-brand text-6xl">{{ route.params.choice }}</h2>
-      <ul>
-        <li v-for="(value, country) in storeStudy.countryData.value" :key="country" class="flex justify-between mx-48 xl:mx-96 my-6 text-brand text-xl">
+      <h2 class="flex justify-center text-brand text-6xl xl:text-7xl 2xl:text-8xl mt-4 xl:mt-8">{{ route.params.choice }}</h2>
+      <ul class="mt-20 lg:mt-28">
+        <li v-for="(value, country) in storeStudy.countryData.value" :key="country" class="flex mx-32 md:mx-40 lg:mx-56 xl:mx-72 2xl:mx-[500px] justify-between my-6 text-brand text-xl xl:text-2xl 2xl:text-3xl">
           <div>{{ country }}:</div> 
           <div>{{ value }}</div>
         </li>
